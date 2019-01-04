d259d75d01ab8330ade723c4ba257dd7dad48704
Azamat <azamat00189@mail.ru>
2019-01-04 21:25:07 +0500

    update

---
+++ src/test/java/uz/coder/readinglist/ReadingListApplicationTests.java
@@ -0,0 +1,17 @@
+package uz.coder.readinglist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReadingListApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
